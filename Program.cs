53193d52736aefee1e7ae4861b28fcbefbafb9c6
Siddhant <santoshshrivastava39@gmail.com>
2022-02-27 22:42:42 +0530

    Added comments

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 
 namespace EmployeeWageProblem
 {
+    /// <summary>
+    /// Contains the Main Method
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Displaying the Greeting Message.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
